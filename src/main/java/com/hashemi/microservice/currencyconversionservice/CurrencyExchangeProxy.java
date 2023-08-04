19e51e29ce5ae9b1a839ff7f95fee4d276dcdbfa
mohammad hashemi <mohammad.hashemi747@gmail.com>
2023-08-04 12:44:07 +0330

    add load balancing

--- src/main/java/com/hashemi/microservice/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/hashemi/microservice/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+// to add load balancing just remove the url to tell feign client to call service
+// based on name and eureka naming server
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
